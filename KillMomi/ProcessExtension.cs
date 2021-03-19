3b6cc4e19cb8f9c77f69e2708d79797b012549e3
Lee Soo Hyuk <30382481+cg10036@users.noreply.github.com>
2021-03-19 10:52:37 +0900

    Add files via upload

---
+++ KillMomi/ProcessExtension.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace KillMomi
+{// Token: 0x02000005 RID: 5
+	public static class ProcessExtension
+	{
+		// Token: 0x0600002E RID: 46
+		[DllImport("kernel32.dll")]
+		private static extern IntPtr OpenThread(ProcessExtension.ThreadAccess A_0, bool A_1, uint A_2);
+
+		// Token: 0x0600002F RID: 47
+		[DllImport("kernel32.dll")]
+		private static extern uint SuspendThread(IntPtr A_0);
+
+		// Token: 0x06000030 RID: 48
+		[DllImport("kernel32.dll")]
+		private static extern int ResumeThread(IntPtr A_0);
+
+		// Token: 0x06000031 RID: 49
+		[DllImport("kernel32.dll")]
+		public static extern int TerminateProcess(IntPtr hProcess, int ExitCode);
+
+		// Token: 0x06000032 RID: 50
+		[DllImport("kernel32.dll")]
+		public static extern void CloseHandle(IntPtr hProcess);
+
+		// Token: 0x06000033 RID: 51
+		[DllImport("kernel32.dll")]
+		private static extern bool TerminateThread(IntPtr A_0, uint A_1);
+
+		// Token: 0x06000034 RID: 52
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+		public static extern IntPtr OpenProcess(int Access, bool InheritHandle, int ProcessId);
+
+		// Token: 0x06000035 RID: 53
+		[DllImport("user32.dll")]
+		private static extern bool PostMessage(IntPtr A_0, uint A_1, int A_2, int A_3);
+
+		// Token: 0x06000036 RID: 54
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern bool PostThreadMessage(uint threadId, uint msg, UIntPtr wParam, IntPtr lParam);
+
+		// Token: 0x06000037 RID: 55 RVA: 0x00004AA8 File Offset: 0x00002CA8
+		public static void Suspend(Process process)
+		{
+			foreach (object obj in process.Threads)
+			{
+				ProcessThread processThread = (ProcessThread)obj;
+				try
+				{
+					IntPtr intPtr = ProcessExtension.OpenThread(ProcessExtension.ThreadAccess.SUSPEND_RESUME, false, (uint)processThread.Id);
+					ProcessExtension.SuspendThread(intPtr);
+					ProcessExtension.CloseHandle(intPtr);
+				}
+				catch
+				{
+				}
+			}
+		}
+
+		// Token: 0x06000038 RID: 56 RVA: 0x00004B30 File Offset: 0x00002D30
+		public static void Resume(Process process)
+		{
+			foreach (object obj in process.Threads)
+			{
+				ProcessThread processThread = (ProcessThread)obj;
+				try
+				{
+					IntPtr intPtr = ProcessExtension.OpenThread(ProcessExtension.ThreadAccess.SUSPEND_RESUME, false, (uint)processThread.Id);
+					ProcessExtension.ResumeThread(intPtr);
+					ProcessExtension.CloseHandle(intPtr);
+				}
+				catch
+				{
+				}
+			}
+		}
+
+		// Token: 0x06000039 RID: 57 RVA: 0x00004BB8 File Offset: 0x00002DB8
+		public static void Kills(Process process)
+		{
+			foreach (object obj in process.Threads)
+			{
+				ProcessThread processThread = (ProcessThread)obj;
+				try
+				{
+					IntPtr intPtr = ProcessExtension.OpenThread(ProcessExtension.ThreadAccess.TERMINATE, false, (uint)processThread.Id);
+					ProcessExtension.TerminateThread(intPtr, 0U);
+					ProcessExtension.CloseHandle(intPtr);
+					ProcessExtension.PostThreadMessage((uint)processThread.Id, ProcessExtension.a, UIntPtr.Zero, IntPtr.Zero);
+				}
+				catch
+				{
+				}
+			}
+			try
+			{
+				IntPtr hProcess = ProcessExtension.OpenProcess(1, false, process.Id);
+				ProcessExtension.TerminateProcess(hProcess, 0);
+				ProcessExtension.CloseHandle(hProcess);
+				ProcessExtension.PostMessage(process.MainWindowHandle, ProcessExtension.a, 0, 0);
+				process.CloseMainWindow();
+				process.Kill();
+			}
+			catch
+			{
+			}
+		}
+
+		// Token: 0x04000019 RID: 25
+		private static uint a = 16U;
+
+		// Token: 0x0400001A RID: 26
+		public const int PROCESS_TERMINATE = 1;
+
+		// Token: 0x02000006 RID: 6
+		public enum ThreadAccess
+		{
+			// Token: 0x0400001C RID: 28
+			TERMINATE = 1,
+			// Token: 0x0400001D RID: 29
+			SUSPEND_RESUME,
+			// Token: 0x0400001E RID: 30
+			GET_CONTEXT = 8,
+			// Token: 0x0400001F RID: 31
+			SET_CONTEXT = 16,
+			// Token: 0x04000020 RID: 32
+			SET_INFORMATION = 32,
+			// Token: 0x04000021 RID: 33
+			QUERY_INFORMATION = 64,
+			// Token: 0x04000022 RID: 34
+			SET_THREAD_TOKEN = 128,
+			// Token: 0x04000023 RID: 35
+			IMPERSONATE = 256,
+			// Token: 0x04000024 RID: 36
+			DIRECT_IMPERSONATION = 512
+		}
+	}
+}
---
+++ KillMomi/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace KillMomi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("잠시만 기다려주세요...");
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.System)) + "Windows");
+                FileInfo[] files = directoryInfo.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+                foreach (FileInfo fileInfo in files)
+                {
+                    if (Path.GetFileNameWithoutExtension(fileInfo.FullName).ToLower().StartsWith("processhide"))
+                    {
+                        try
+                        {
+                            fileInfo.Attributes = FileAttributes.Normal;
+                            string fullName = fileInfo.FullName;
+                            if (Path.GetExtension(fullName).Contains("exe"))
+                            {
+                                new FileInfo(Directory.GetCurrentDirectory() + "\\Command.exe")
+                                {
+                                    Attributes = FileAttributes.Normal
+                                }.CopyTo(fullName, true);
+                                Console.WriteLine("Copy Success");
+                            }
+                            if (Path.GetExtension(fullName).Contains("dll"))
+                            {
+                                new FileInfo(Directory.GetCurrentDirectory() + "\\CommandDLL.dll")
+                                {
+                                    Attributes = FileAttributes.Normal
+                                }.CopyTo(fullName, true);
+                                Console.WriteLine("Copy Success");
+                            }
+                            FileInfo fileInfo2 = new FileInfo(fullName);
+                            fileInfo2.Attributes = (FileAttributes.ReadOnly | FileAttributes.Hidden | FileAttributes.System);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
+                        }
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            List<string> lists = new List<string>();
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.System)) + "Windows");
+                FileInfo[] files = directoryInfo.GetFiles("*.exe", SearchOption.TopDirectoryOnly);
+                foreach (FileInfo fileInfo2 in files)
+                {
+                    try
+                    {
+                        string fullName = fileInfo2.FullName;
+                        X509Certificate2 x509Certificate = new X509Certificate2(File.ReadAllBytes(fullName));
+                        string subject = x509Certificate.Subject;
+                        if (subject.Contains("JNESS") || subject.Contains("제이니스"))
+                        {
+                            lists.Add(fullName);
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.System)) + "Windows\\SysWOW64");
+                FileInfo[] files = directoryInfo.GetFiles("*.exe", SearchOption.TopDirectoryOnly);
+                foreach (FileInfo fileInfo2 in files)
+                {
+                    try
+                    {
+                        string fullName = fileInfo2.FullName;
+                        X509Certificate2 x509Certificate = new X509Certificate2(File.ReadAllBytes(fullName));
+                        string subject = x509Certificate.Subject;
+                        if (subject.Contains("JNESS") || subject.Contains("제이니스"))
+                        {
+                            lists.Add(fullName);
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.System)) + "Windows\\System32");
+                FileInfo[] files = directoryInfo.GetFiles("*.exe", SearchOption.TopDirectoryOnly);
+                foreach (FileInfo fileInfo2 in files)
+                {
+                    try
+                    {
+                        string fullName = fileInfo2.FullName;
+                        X509Certificate2 x509Certificate = new X509Certificate2(File.ReadAllBytes(fullName));
+                        string subject = x509Certificate.Subject;
+                        if (subject.Contains("JNESS") || subject.Contains("제이니스"))
+                        {
+                            lists.Add(fullName);
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine(lists.Count);
+            foreach (string path in lists)
+            {
+                foreach (Process process in Process.GetProcesses())
+                {
+                    if (process.ProcessName.StartsWith(Path.GetFileNameWithoutExtension(path)))
+                    {
+                        try
+                        {
+                            ProcessExtension.Kills(process);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("완료되었습니다.");
+            Console.ReadLine();
+        }
+    }
+}
